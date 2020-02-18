610529174d43932bd3ddcff78ff53594aa4edba9
jbharath46 <61136691+jbharath46@users.noreply.github.com>
2020-02-18 19:01:45 +0530

    Update TestController.java

--- src/main/java/springboot/webservice/TestController.java
+++ src/main/java/springboot/webservice/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-@RequestMapping("/")
+@RequestMapping("/home")
 public String home() {
 return "Maven build with openshift!";
 }
