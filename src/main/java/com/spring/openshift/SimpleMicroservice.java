92cb4cd37b7a1cea246d0d262be6dc6c7d4c687f
jbharath46 <61136691+jbharath46@users.noreply.github.com>
2020-03-03 15:18:30 +0530

    Update SimpleMicroservice.java

--- src/main/java/com/spring/openshift/SimpleMicroservice.java
+++ src/main/java/com/spring/openshift/SimpleMicroservice.java
@@ -12,12 +12,12 @@ public class SimpleMicroservice{
 
 	@GetMapping("/")
 	public String welcome() {
-		return "<font size=10><b>Introduction to OpenShift Container Platform </b></font> <br><br><br><br><br><font size=6><p>OpenShift Container Platform is a platform for developing and running containerized applications. It is designed to allow applications and the data centers that support them to expand from just a few machines and applications to thousands of machines that serve millions of clients.With its foundation in Kubernetes, OpenShift Container Platform incorporates the same technology that serves as the engine for massive telecommunications, streaming video, gaming, banking, and other applications. Its implementation in open Red Hat technologies lets you extend your containerized applications beyond a single cloud to on-premise and multi-cloud environments</p></font>";
+		return "<font size=6><b>Introduction to OpenShift Container Platform </b></font> <br><br><br><br><br><font size=5><p>OpenShift Container Platform is a platform for developing and running containerized applications. It is designed to allow applications and the data centers that support them to expand from just a few machines and applications to thousands of machines that serve millions of clients.With its foundation in Kubernetes, OpenShift Container Platform incorporates the same technology that serves as the engine for massive telecommunications, streaming video, gaming, banking, and other applications. Its implementation in open Red Hat technologies lets you extend your containerized applications beyond a single cloud to on-premise and multi-cloud environments</p></font>";
 	}
 
 	@GetMapping("/{input}")
 	public String congrats(@PathVariable String input) {
-		return "<font size=7>Hello</font>" + input + "<font size=7>,!! <br><br><br><br>  <b> Build and deployment successfull.</b></font>";
+		return "<font size=6>Hello" + input + ",!! <br><br><br><br>  <b> Build and deployment successfull.</b></font>";
 	}
 
 	public static void main(String[] args) {
