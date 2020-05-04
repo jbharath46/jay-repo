12225a6b2dad131f92e02b954112e6a3c658b938
jbharath46 <61136691+jbharath46@users.noreply.github.com>
2020-05-04 15:56:22 +0530

    Update SimpleMicroservice.java

--- src/main/java/com/spring/openshift/SimpleMicroservice.java
+++ src/main/java/com/spring/openshift/SimpleMicroservice.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleMicroservice{
 
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String welcome() {
-		return "<font size=6><b>Introduction to OpenShift Container Platform </b></font> <br><br><br><br><br><font size=5><p>OpenShift Container Platform is a platform for developing and running containerized applications. It is designed to allow applications and the data centers that support them to expand from just a few machines and applications to thousands of machines that serve millions of clients.With its foundation in Kubernetes, OpenShift Container Platform incorporates the same technology that serves as the engine for massive telecommunications, streaming video, gaming, banking, and other applications. Its implementation in open Red Hat technologies lets you extend your containerized applications beyond a single cloud to on-premise and multi-cloud environments</p></font>";
+		return "<font size=6><b>Welcome !</b></font>";
 	}
 
 	@GetMapping("/{input}")
